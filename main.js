b0ea0f6d482dd386d1f7ef472a31f6baab31e5ee
nirglus <nirgluskin@gmail.com>
2023-09-19 21:23:57 +0300

    adjust validate func

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 function validate(){
+    let userMail = document.getElementById("userMail");
     let endsWithCom = userMail.value.lastIndexOf(".com") == userMail.value.length - 4;
-    let hasDot = userMail.indexOf(".") != -1;
-    let hasMail = userMail.indexOf("@") != -1;
+    let hasDot = userMail.value.indexOf(".") != -1;
+    let hasMail = userMail.value.indexOf("@") != -1;
     if(endsWithCom && hasDot && hasMail){
+        document.querySelector(".error").classList.add("hidden");
         return true;
     }else{
-        
+        userMail.style.border ="1px solid hsl(354, 100%, 66%)";
+        document.querySelector(".error").classList.remove("hidden");
+        return false;
     }
 }
