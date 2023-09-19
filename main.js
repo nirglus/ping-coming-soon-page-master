b4b954948109b77d0add262ab0f3985fb6ec1588
nirglus <nirgluskin@gmail.com>
2023-09-19 20:55:31 +0300

    add error span

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-
+function validate(){
+    let endsWithCom = userMail.value.lastIndexOf(".com") == userMail.value.length - 4;
+    let hasDot = userMail.indexOf(".") != -1;
+    let hasMail = userMail.indexOf("@") != -1;
+    if(endsWithCom && hasDot && hasMail){
+        return true;
+    }else{
+        
+    }
+}
