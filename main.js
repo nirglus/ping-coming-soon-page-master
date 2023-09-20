0e4a1a9c540c58029679251fa2c08216ddffb2e9
nirglus <nirgluskin@gmail.com>
2023-09-20 10:19:52 +0300

    add errorShake anim

--- main.js
+++ main.js
@@ -5,10 +5,15 @@ function validate(){
     let hasMail = userMail.value.indexOf("@") != -1;
     if(endsWithCom && hasDot && hasMail){
         document.querySelector(".error").classList.add("hidden");
+        userMail.classList.remove("errorShake");
         return true;
     }else{
         userMail.style.border ="1px solid hsl(354, 100%, 66%)";
         document.querySelector(".error").classList.remove("hidden");
+        userMail.classList.add("errorShake");
+        setTimeout(() => {
+            userMail.classList.remove("errorShake");
+        }, 300)
         return false;
     }
 }
